cefaa5f2a0f1ad9ea74968c1211958a08ffca44f
Anna <turbina.anna97@yandex.ru>
2022-12-25 14:33:17 +0100

    Final code

---
+++ Program/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите через пробел любые элементы массива");
+string s = Console.ReadLine();
+string[] line = s.Split(' ');
+string[] array = new string[line.Length];
+
+void ThreeCharElements(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = line[i];
+        if (array[i].Length <= 3)
+            Console.Write($"{array[i]} ");
+    }
+}
+ThreeCharElements(array);
